c3fd7a097ac5d799917fac07fa4e7b864388b4d7
GabrielVall <gabrielvallejo2000@gmail.com>
2022-07-23 16:00:36 -0500

    guardar num en local storage

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
             });
             
             if(cbx.checked) {
-                // localStorage.setItem('celular', cel);
+                localStorage.setItem('celular', cel);
             }
             
         }else{
