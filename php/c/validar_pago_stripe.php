3d0bdb58e97c314cb19d05448d670df932d11468
GabrielVall <gabrielvallejo2000@gmail.com>
2022-07-28 15:20:52 -0500

    validar stripe

--- php/c/validar_pago_stripe.php
+++ php/c/validar_pago_stripe.php
@@ -6,6 +6,15 @@ $sql = new SQLConexion();
 // Obtener la Secret Key de Stripe
 $select_keys = $sql->obtenerResultado("CALL sp_select_keys()");
 $secret_stripe = $select_keys[2]['valor_configuracion'];
+$validar_rastreo = $sql->obtenerResultado("CALL sp_select_rastreo('".$_SESSION['stripe_session_id']."')");
+if($validar_rastreo[0][0] > 0){
+  echo json_encode(
+    array(
+        'status' => 'pagado',
+    )
+  );
+  exit;
+}
 
 require '../../stripe/vendor/autoload.php';
 \Stripe\Stripe::setApiKey($secret_stripe);
@@ -18,8 +27,12 @@ if($return['status'] == 'complete'){
     
     // Sleccionamos la linea telefonica por el ID de la SIM
     $id_linea = $sql->obtenerResultado("SELECT fn_select_linea_telefonica('".$SIM."')");
-    $pagar = $sql->obtenerResultadoSimple("CALL sp_insert_contratos_lineas_telefonicas1('".$id_linea[0][0]."',3,'".$_SESSION['offer_id']."')");
-    echo terminar_pago_api($_SESSION['offer_id'],$id_linea[0][0]);
+    $pagar = $sql->obtenerResultadoID("CALL sp_insert_contratos_lineas_telefonicas2('".$id_linea[0][0]."',4,'".$_SESSION['offer_id']."',@_ID)");
+    $id_pago = $pagar[0][0];
+    $validar = $sql->obtenerResultadoSimple("CALL sp_insertar_id_rastreo({$id_pago},'".$_SESSION['stripe_session_id']."')");
+    if($validar){
+        echo terminar_pago_api($_SESSION['offer_id'],$id_linea[0][0]);
+    }
 }else{
     echo json_encode(
         array(
