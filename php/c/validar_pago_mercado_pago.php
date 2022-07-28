429357b65bb219b2cae8fcab6e48f7670393f91b
GabrielVall <gabrielvallejo2000@gmail.com>
2022-07-28 11:52:16 -0500

    cambio var

--- php/c/validar_pago_mercado_pago.php
+++ php/c/validar_pago_mercado_pago.php
@@ -5,7 +5,7 @@ $sql = new SQLConexion();
 // Obtener la Secret Key de MercadoPago
 $select_keys = $sql->obtenerResultado("CALL sp_select_keys()");
 $secret_mp = $select_keys[3]['valor_configuracion'];
-$validar_rastreo = $sql->obtenerResultado("CALL sp_select_rastreo({$_GET['id_rastreo']})");
+$validar_rastreo = $sql->obtenerResultado("CALL sp_select_rastreo({$_POST['payment_id']})");
 if($validar_rastreo[0][0] > 0){
   echo '<script>
   alert("Tu pago ya fue procesado");
